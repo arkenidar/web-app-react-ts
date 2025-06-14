e8d95168d05c8306f04812292a41c21d99bd9d8a
Dario Cangialosi <dario.cangialosi@gmail.com>
2025-06-14 21:07:45 +0200

    chore: add build instructions as comments in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // ln -s docs web-app-react-ts
+  // npm run build
   build: {
     outDir: 'docs',
   },
