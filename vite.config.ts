25963e9bfc4ef7cd6a5256669f39cfd8b8a216d5
Dario Cangialosi <dario.cangialosi@gmail.com>
2025-06-14 20:56:56 +0200

    Add initial HTML, CSS, and SVG assets for the web application
    - Created index.html with basic structure and linked assets
    - Added index-Dtn62Xmo.css for styling the application
    - Included vite.svg as the favicon for the application

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dist/',
+  build: {
+    outDir: 'web-app-react-ts',
+  },
+  base: '/web-app-react-ts/',
 })
